376f8d17167ebfa449e574df194678635bfea952
Victoria Gonzalez <gonzalezevic@gmail.com>
2022-03-09 11:13:31 -0800

    vg-phase-1-project

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
+const cocktailSearch = 'https://www.thecocktaildb.com/api/json/v1/1/random.php'
+const ingredients = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?i='
 
+fetch (cocktailSearch)
+.then(res => res.json())
+.then(data => console.log(data))
+
+//getting elements
+document.getElementById() 
+document.getElementById()
+
+// events 
+const random= document.addEventListener('click', handleClick)
+const details= document.addEventListener('submit', handleSubmit)
+
+function handleClick(){
+
+}
+
+function handleSubmit(e){
+e.preventDefault()
+}
+// give me a random cocktail
+function randomCocktail() {
+
+}
+
+//give me ingredients 
+function cocktailIngredients(){
+
+}
+
+
+
+
+//preventDefault must be in the handle submit 
