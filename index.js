2cf96f4582b2b9de285d9f9d85e300b531909d7b
Victoria Gonzalez <gonzalezevic@gmail.com>
2022-03-13 21:18:45 -0700

    vg-phase-1-project

--- index.js
+++ index.js
@@ -1,40 +1,43 @@
-const cocktailSearch = 'https://www.thecocktaildb.com/api/json/v1/1/random.php'
+const randomSearch = 'https://www.thecocktaildb.com/api/json/v1/1/random.php'
 const ingredients = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?i='
 
-fetch (cocktailSearch)
-.then(res => res.json())
-.then(data => console.log(data))
+const button= document.getElementById('randombutton')
+const img =document.getElementById('image')
 
 fetch(ingredients)
 .then(res => res.json())
 .then(data => console.log(data))
 
-// //getting elements
-// document.getElementById() 
-// document.getElementById()
+fetch (randomSearch)
+.then(res => res.json())
+.then(data => console.log(data))
 
-// // events 
-// const random= document.addEventListener('click', handleClick)
+// my click button function
+const random= document.addEventListener('click', handleClick)
+ function handleClick(button){
+ }
+ //give me a random cocktail
+function randomCocktail() {
+    
+}
+
+
+
+
+
+
+
+
 // const details= document.addEventListener('submit', handleSubmit)
-
-// function handleClick(){
-
-// }
 
 // function handleSubmit(e){
 // e.preventDefault()
 // }
-// // give me a random cocktail
-// function randomCocktail() {
 
-// }
 
 // //give me ingredients 
 // function cocktailIngredients(){
 
 // }
-
-
-
 
 //preventDefault must be in the handle submit 
